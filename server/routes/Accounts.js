67e950452d8a2c8858bbedda0c57b06a05313ea6
iykeevans <elochi238@gmail.com>
2019-04-03 12:10:44 +0100

    feat (API): implement delete acount [delivers #165041109]

--- server/routes/Accounts.js
+++ server/routes/Accounts.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createAccount } = require('../controllers/Accounts');
+const { createAccount, deleteAccount } = require('../controllers/Accounts');
 
 const router = express.Router();
 
 router.post('/', createAccount);
+router.delete('/:accountNumber', deleteAccount);
 
 module.exports = router;
--- test/accounts.js
+++ test/accounts.js
@@ -6,30 +6,28 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 // Account test
-describe('Account test suite', () => {
+describe('Create account test suite', () => {
   // create account test
-  describe('Create Account suite', () => {
-    it('should create an account', (done) => {
-      const values = {
-        email: 'fluxie@gmail.com',
-        firstName: 'flux',
-        lastName: 'mel',
-        accountNumber: 6171257181,
-        type: 'savings',
-      };
+  it('should create an account', (done) => {
+    const values = {
+      email: 'fluxie@gmail.com',
+      firstName: 'flux',
+      lastName: 'mel',
+      accountNumber: 6171257000,
+      type: 'savings',
+    };
 
-      chai
-        .request(app)
-        .post('/api/v1/accounts')
-        .send(values)
-        .end((err, res) => {
-          expect(res.status).to.equal(201);
-          expect(res.body.status).to.equal(201);
-          expect(res.body).to.have.property('data');
-          expect(res.body.data).to.be.a('object');
-          done();
-        });
-    });
+    chai
+      .request(app)
+      .post('/api/v1/accounts')
+      .send(values)
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        expect(res.body.status).to.equal(201);
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.a('object');
+        done();
+      });
   });
 
   // test validation
@@ -38,7 +36,7 @@ describe('Account test suite', () => {
       email: 'fluxiegmail.com',
       firstName: 'flux',
       lastName: 'mel',
-      accountNumber: 6171257181,
+      accountNumber: 6171257000,
       type: 'savings',
     };
 
@@ -46,6 +44,35 @@ describe('Account test suite', () => {
       .request(app)
       .post('/api/v1/accounts')
       .send(values)
+      .end((err, res) => {
+        expect(res.status).to.equal(500);
+        expect(res.body.status).to.equal(500);
+        expect(res.body).to.have.property('error');
+        done();
+      });
+  });
+});
+
+describe('Delete account test suite', () => {
+  // delete account test
+  it('should delete an account', (done) => {
+    chai
+      .request(app)
+      .delete('/api/v1/accounts/6171257000')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.status).to.equal(200);
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.equal('Account successfully deleted');
+        done();
+      });
+  });
+
+  // test validation
+  it('should return a delete account error', (done) => {
+    chai
+      .request(app)
+      .delete('/api/v1/accounts/6171257144')
       .end((err, res) => {
         expect(res.status).to.equal(500);
         expect(res.body.status).to.equal(500);
