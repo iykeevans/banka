14cfbdd2e529c22a8bfe8ba9a399b0e0ddd8c678
iykeevans <elochi238@gmail.com>
2019-04-18 15:06:11 +0100

    feat (API): implement view specific transaction [delivers #165484394]

--- server/routes/Transactions.js
+++ server/routes/Transactions.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import transaction from '../controllers/Transactions';
+import { createTransaction, getTransaction } from '../controllers/Transactions';
 import verifyToken from '../middlewares/authentication/verifyToken';
 import isStaff from '../middlewares/authentication/staffAuth';
 
 const router = express.Router();
 
-router.post('/:accountNumber/debit', verifyToken, isStaff, transaction);
-router.post('/:accountNumber/credit', verifyToken, isStaff, transaction);
+router.get('/:id', verifyToken, getTransaction);
+router.post('/:accountNumber/debit', verifyToken, isStaff, createTransaction);
+router.post('/:accountNumber/credit', verifyToken, isStaff, createTransaction);
 
 export default router;
